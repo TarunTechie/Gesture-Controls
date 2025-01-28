2127e681265ef9ad40c40b9cba0183532be70a07
TarunTechie <taruntechie25@gmail.com>
2025-01-29 00:16:57 +0530

    added ability to set parameters for tracking

--- handtracking.py
+++ handtracking.py
@@ -3,18 +3,22 @@ import cv2
 import time as t
 
 class handDetector():
-    def __init__(self):
+    def __init__(self,mode=False,hands=2,detectCon=0.5,trackCon=0.5):
         self.mpHands=mp.solutions.hands
-        self.hands=self.mpHands.Hands()
+        self.hands=self.mpHands.Hands(static_image_mode=mode,
+                                      max_num_hands=hands,
+                                      min_detection_confidence=detectCon,
+                                      min_tracking_confidence=trackCon)
         self.mpdraw=mp.solutions.drawing_utils        
 
     def findHands(self,img,draw=True):
         img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         self.results=self.hands.process(img_rgb)
         if (self.results.multi_hand_landmarks):
-            for handlm in self.results.multi_hand_landmarks:
+            for handslm in self.results.multi_hand_landmarks:
                 if draw:    
-                    self.mpdraw.draw_landmarks(img,handlm,self.mpHands.HAND_CONNECTIONS)
+                    self.mpdraw.draw_landmarks(img,handslm,self.mpHands.HAND_CONNECTIONS)
+        return img
     
     def handPosition(self,img,handNo=0,draw=True):
         lmList=[]
@@ -23,21 +27,7 @@ class handDetector():
             handlm=self.results.multi_hand_landmarks[handNo]
             for id,lm in enumerate(handlm.landmark):
                 x,y=int(lm.x*w), int(lm.y*h)
-                print(x,'\t',y)
                 lmList.append([id,x,y])
         return lmList
                 
-    
-def main():
-    cam=cv2.VideoCapture(0)
-    dec=handDetector()
-    while True:
-        success,img=cam.read()
-        dec.findHands(img,True)
-        newlist=dec.handPosition(img)
-        cv2.putText(img,str(img.shape),(10,70),cv2.FONT_HERSHEY_SCRIPT_COMPLEX,1.5,(0,0,251),3)
-        cv2.imshow("image",img)
-        cv2.waitKey(1)
 
-
-main()
